27896d2c70f66a1d782565a15dc2b6abcbd20a0b
robertrh3 <robertrh@Roberts-MacBook-Pro.local>
2019-08-19 22:33:51 -0600

    main activity imports

--- baby_names/android/app/src/main/java/com/example/baby_names/MainActivity.java
+++ baby_names/android/app/src/main/java/com/example/baby_names/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.baby_names;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends FlutterActivity {
   @Override
